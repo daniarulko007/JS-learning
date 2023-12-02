adecb0a0d85295bfd084e502fc85802db2a4795f
daniarulko007 <bohdanarulko14@gmail.com>
2023-12-02 23:29:45 +0200

    push

--- src/js/index1.js
+++ src/js/index1.js
@@ -1,2 +1,16 @@
-let str = "Hello world";
-console.log(str);
+// const result = str1 + str2;
+// let x = 123;
+// x = 10;
+
+// let str1 = "Hello world test";
+
+// const strLength = str1.length();
+// const last = str1[strLength - 1];
+// const result = str1[0] + str1[4];
+// console.log(result);
+
+const x = 10;
+const y = 20;
+
+const result = (x >= y) + (x<=y);
+console.log(result);
