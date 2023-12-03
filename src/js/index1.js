89af955ab3ca2a3f5af466005caeb3a25bad6d98
daniarulko007 <bohdanarulko14@gmail.com>
2023-12-03 10:06:21 +0200

    finish-1-2

--- src/js/index1.js
+++ src/js/index1.js
@@ -3,14 +3,85 @@
 // x = 10;
 
 // let str1 = "Hello world test";
-
 // const strLength = str1.length();
 // const last = str1[strLength - 1];
 // const result = str1[0] + str1[4];
 // console.log(result);
 
-const x = 10;
-const y = 20;
+// const x = 10;
+// const y = 20;
+// const result = (x >= y) + (x <= y);
+// console.log(result);
 
-const result = (x >= y) + (x<=y);
-console.log(result);
+// const t = '10';
+// const r = 10;
+// console.log(t === r);
+
+// let x = 20;
+// console.log(x);
+
+// const firstName = 'Stas';
+// const lastName = 'Max';
+// const age = 10;
+// const city = 'Dnipro';
+// const result = 'First name: ' + firstName + '\nLast name: ' + lastName + '\nAge: ' + age;
+// const result = `First name: ${firstName} \nLast name: ${lastName} \nAge: ${age}`;
+// console.log(result);
+
+// function foo() {
+//     console.log('Hello');
+//     console.log('World');
+//     console.log('test');
+// }
+// function foo() {
+//     const str ='Hello world';
+//     console.log(str);
+// }
+// foo();
+// console.log('-----');
+// foo();
+// console.log('-----');
+// foo();
+
+// function pow(x, y){
+//     console.log(x ** y);
+// }
+// pow(2, 4);
+// pow(2, 6);
+// pow(3, 4);
+
+// const x = 10;
+// const y = 50;
+// function foo1(){
+//     const x = 100;
+//     console.log(x,y);
+// }
+// function foo2(){
+//     const x = 200;
+//     const y = 300;
+//     console.log(x, y);
+// }
+// function foo3(){
+//     const y = 20;
+//     console.log(x, y);
+// }
+// foo1();
+// foo2();
+// foo3();
+
+// function foo() {
+//     console.log(1);
+//     console.log(2);
+//     return 3;
+//     console.log(4);
+//     console.log(5);
+//     return 10;
+// }
+// foo();
+
+// function foo(x, y) {
+//     return x - y;
+// }
+// debugger;
+// const result = Math.pow(foo(5,3), foo(2,1));
+// console.log (result);
